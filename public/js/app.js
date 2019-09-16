f959ecae1edd317dd7e80695194ecab216756838
DANIELS-ROTH <contact@daniels-roth-stan.fr>
2019-09-16 17:53:42 +0200

    added debug

--- public/js/app.js
+++ public/js/app.js
@@ -381,11 +381,12 @@ class TableApp
 			{
 				let container = (ui.helper).parents("tbody")
 				let position = ui.position.top - $(ui.helper).parents("tbody").offset().top;
-				console.log(position);
-				container.find("td").each(function()
-				{
-					console.log(this);
-				});
+				console.log("position % 46 = "+ (position % 46));
+//				container.find("td").each(function()
+//				{
+//					if ()
+//					console.log(this);
+//				});
 //				let elementOver = 
 			},
 		});
