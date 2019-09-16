b6ca1a61331bc73310e3e43f34b41157f7a55b8b
DANIELS-ROTH <contact@daniels-roth-stan.fr>
2019-09-16 18:29:45 +0200

    fixed variable value

--- resources/views/parametres/node.blade.php
+++ resources/views/parametres/node.blade.php
@@ -1,7 +1,7 @@
 @extends ("layout")
 @section ("content")
 	<section id="dashboard" class="">
-		@component('arrayViewer', ["title" => $title, "url" => $node, "foldable" => $foldable])
+		@component('arrayViewer', ["title" => $title, "url" => $node, "foldable" => false])
 			<strong>Whoops!</strong> Une erreur s'est produite !
 		@endcomponent
 	</section>
