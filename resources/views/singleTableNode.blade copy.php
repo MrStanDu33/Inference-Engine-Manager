5dfa8ed6e5c9acacae18e16510ffdcb1fac43747
DANIELS-ROTH <contact@daniels-roth-stan.fr>
2019-09-19 19:03:35 +0200

    added structure to prepare side navbar integration

--- resources/views/singleTableNode.blade copy.php
+++ resources/views/singleTableNode.blade copy.php
@@ -1,11 +1,6 @@
 @extends ("layout")
 @section ("content")
 	<section id="dashboard tableApp" class="">
-		@if ($navigation)
-			@component('navViewer', ["title" => $title, "url" => $node, "foldable" => false])
-				<strong>Whoops!</strong> Une erreur s'est produite !
-			@endcomponent
-		@endif
 		@component('arrayViewer', ["title" => $title, "url" => $node, "foldable" => false])
 			<strong>Whoops!</strong> Une erreur s'est produite !
 		@endcomponent
