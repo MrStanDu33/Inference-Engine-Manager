1215d369f162c8ef0aa124786df473c628fd8bf0
DANIELS-ROTH Stan <contact@daniels-roth-stan.fr>
2019-10-26 19:39:05 +0200

    removed facde

--- config/app.php
+++ config/app.php
@@ -225,7 +225,6 @@ return [
 		'Validator' => Illuminate\Support\Facades\Validator::class,
 		'View' => Illuminate\Support\Facades\View::class,
 
-		'Debugbar' => Barryvdh\Debugbar\Facade::class,
 	],
 
 ];
