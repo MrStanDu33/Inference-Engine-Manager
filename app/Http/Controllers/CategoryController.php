705c1262f15c2226e4d5cdfe1d71f5a1abbb83f2
DANIELS-ROTH Stan <contact@daniels-roth-stan.fr>
2019-10-31 07:43:08 +0100

    fixed import for eloquentRequests

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Category;
 use App\CustomLibrary\HttpResponseCode;
 use App\Http\Controllers\Controller;
+use App\Traits\eloquentRequests;
 use Validator;
 
 class CategoryController extends Controller
