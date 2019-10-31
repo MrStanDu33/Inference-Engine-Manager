7359f5341061194f9611d2a3668dbdd2ef5fbe92
DANIELS-ROTH Stan <contact@daniels-roth-stan.fr>
2019-10-31 16:09:54 +0100

    added debug

--- public/js/netstableNavBar.js
+++ public/js/netstableNavBar.js
@@ -35,9 +35,13 @@ class nestableNavBar
 			if (!!element.referral)
 			{
 				if (root.find("li[data-id=\""+element.referral+"\"]>ul").length === 0)
+				{
 					let tempRoot = root.find("li[data-id=\""+element.referral+"\"]").append($("<ul></ul>"));
+				}
 				else
+				{
 					tempRoot = root.find("li[data-id=\""+element.referral+"\"]>ul");
+				}
 				tempRoot.append($("<li data-id=\""+element.id+"\" class=\"navNestableLine\">"+element.name+"</li>"));
 			}
 			else
